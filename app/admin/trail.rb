7e3d9f32bde5af3296af84ba9a14b9132578d111
Raghu Betina <raghu@firstdraft.com>
2017-03-02 20:37:17 +0000

    Whitelist columns in Trail for ActiveAdmin

--- app/admin/trail.rb
+++ app/admin/trail.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Trail do
 
+ permit_params :trail_name, :mountain_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
