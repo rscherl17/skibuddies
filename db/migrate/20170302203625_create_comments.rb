34d365ef173944bf543eacfcac14a3851057e986
Raghu Betina <raghu@firstdraft.com>
2017-03-02 20:36:25 +0000

    Generate comments resource with starter:resource

---
+++ db/migrate/20170302203625_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :comment
+      t.integer :user_id
+      t.integer :trail_id
+
+      t.timestamps
+
+    end
+  end
+end
