c2bf96c614d7b0e90b2a35debe4e4d0bd6dbd2ee
Raghu Betina <raghu@firstdraft.com>
2017-03-02 20:36:48 +0000

    Direct association between users and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :counter_cache => true
+
   belongs_to :mountain,
              :counter_cache => true
 
